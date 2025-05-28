157fe0194ccb042ff400f3a372712e11d8ce42b3
nacycher <nacycher@163.com>
2025-05-28 16:07:26 +0800

    init project

---
+++ src/main/java/com/tools/studyflowable/StudyFlowableApplication.java
@@ -0,0 +1,13 @@
+package com.tools.studyflowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyFlowableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tools/studyflowable/StudyFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tools.studyflowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
