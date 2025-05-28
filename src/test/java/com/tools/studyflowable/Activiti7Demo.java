0804be6b7f0a08ee92c72291b4988d8f7bbb600a
nacycher <nacycher@163.com>
2025-05-28 17:47:43 +0800

    [1.3]获取ProcessEngine测试用例

--- src/test/java/com/tools/studyflowable/Activiti7Demo.java
+++ src/test/java/com/tools/studyflowable/Activiti7Demo.java
@@ -1,6 +1,7 @@
 package com.tools.studyflowable;
 
 import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngines;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,22 @@ public class Activiti7Demo {
     public void getProcessEngine(){
         // 获取默认的ProcessEngine对象，会加载resource目录下的activiti.cfg.xml文件
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        System.out.println(processEngine);
+    }
+
+    /**
+     * 通过ProcessEngineConfiguration创建ProcessEngine对象
+     */
+    @Test
+    public void getProcessEngine2(){
+        ProcessEngine processEngine = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:mysql://localhost:3306/activiti7")
+                .setJdbcDriver("com.mysql.cj.jdbc.Driver")
+                .setJdbcUsername("root")
+                .setJdbcPassword("root")
+                .setDatabaseSchemaUpdate("true")// 设置自动维护表结构
+                .buildProcessEngine();
+
         System.out.println(processEngine);
     }
 }
